b2a7bd27bc63e2b488df29674f489a4669e90774
Henrique Jensen <henriquejensen@hotmail.com>
2016-09-28 13:02:35 -0300

    iniciando aplicacao com as info do problema

---
+++ src/br/com/main/Stream.java
@@ -0,0 +1,7 @@
+package br.com.main;
+
+public interface Stream {
+	public char getNext();
+
+	public boolean hasNext();
+}
---
+++ src/br/com/main/StreamImpl.java
@@ -0,0 +1,12 @@
+package br.com.main;
+
+public class StreamImpl implements Stream {
+	public char getNext() {
+		return 'A';
+	}
+
+	public boolean hasNext() {
+		return false;
+	}
+
+}
